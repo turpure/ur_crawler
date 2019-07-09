18e1395c538458b17eac77a344a5dd3b929d7c79
turpure <p812816627@163.com>
2019-07-09 10:16:27 +0800

    tasks queue

---
+++ src/add_task.py
@@ -0,0 +1,35 @@
+#! usr/bin/env/python3
+# coding:utf-8
+# @Time: 2019-07-08 16:02
+# Author: turpure
+
+from config.conf import info
+from tasks import get_joom_product_by_category
+import redis
+
+
+def add_task(cate_id, page_token):
+    # id = '1473502940450448049-189-2-118-805240694'
+    res = get_joom_product_by_category.delay(cate_id, page_token)
+
+
+def get_task():
+    rd = redis.Redis(**info['redis']['task'])
+    while True:
+        task = rd.blpop('joom_task', 30)
+        if not task:
+            continue
+        print('get task {} from task queue'.format(task[1]))
+        task_info = task[1].decode('utf-8').split(',')
+        print(task_info)
+        cate_id = task_info[0]
+        page_token = task_info[1]
+        add_task(cate_id, page_token)
+
+
+if __name__ == '__main__':
+    get_task()
+    # rd = redis.Redis(**info['redis']['task'])
+    # rd.lpush('joom_task', ','.join(['fef', 'test' ]))
+
+
--- src/tasks.py
+++ src/tasks.py
@@ -6,35 +6,43 @@
 import requests
 from celery import Celery
 import json
+from config.conf import info
+import redis
 
 
-app = Celery('tasks', broker='redis://''@47.88.7.50:6380/1', backend='redis://''@127.0.0.1:6379/5')
+app = Celery('tasks', broker=info['redis']['broker'], backend=info['redis']['backend'])
+rd = redis.Redis(**info['redis']['task'])
 
 
 @app.task
-def get_joom_product(category_id):
+def get_joom_product_by_category(category_id, page_token=''):
+    global rd
     base_url = 'https://api.joom.com/1.1/search/products?currency=USD&language=en-US&_=jxo2h958'
-    headers = {
-        'Authorization': 'Bearer SEV0001MTU0NjY1Mzc2M3xTSU9XdUJFbFU4NDJ5VHVndk8tV3ROem8yYVFCV0QtYjE2aTBDM3FNLWZkbVFyX01aTFJUek05REJZUVZnWVNmOE5TanlCWXhYRk84MWFINHZDTE5UVUJGb0ZSTmFWLXlkZlRCem9YRVg4R21GSEEwVHNQeHJIUWZKMmJ5dWd2VmpKNkZ4Q0V6VS1JdF9EZzF1UGtyb1NzcVQ5VDlQLTRwNnJ4Nl9yaHZTUkEzUmRfZUI0ZFB1TGxXejFFTkNzNm1PUzZoY1BScXI1YVhEWGlWdmVwODJIOVhxTnlZcGYxSVdDQzJXY1RTQjMyUjRWc09FVVU9fJgmguEcQE9NGiD_vYv4ymZpnsmOBH4btJX1l56WY4V7',
-        'X-API-Token': 'p5F3VmqeHtMfBcvvpwTNVm9mRJuzHzx6',
-        'Content-Type': 'application/javascript'
-    }
-
+    headers = info['headers']
     try:
-        request_data = {"filters":
-                            [{"id": "categoryId",
-                              "value": {"type": "categories",
-                                        "items": [{"id": category_id}]}}], "count": 36}
-        request_data = json.dumps(request_data)
-        ret = requests.post(base_url, headers=headers, data=request_data)
-        return (ret.json()['payload']['items'])
+        request_data = {"filters":[
+            {"id": "categoryId",
+             "value": {"type": "categories",
+                       "items": [{"id": category_id}]}}], "count": 36, 'pageToken': page_token}
+        for i in range(4):
+            try:
+                request_data = json.dumps(request_data)
+                ret = requests.post(base_url, headers=headers, data=request_data)
+                payload = ret.json()['payload']
+                page_token = payload.get('nextPageToken', '')
+                break
+            except:
+               pass
+        rd.lpush('joom_task', ','.join([category_id, page_token]))
+        return ':'.join([category_id, page_token])
     except Exception as why:
-        print(why)
+        return 'fail to get result cause of {}'.format(why)
 
 
 if __name__ == '__main__':
     # print(get_joom_product('1473502940450448049-189-2-118-805240694'))
-    res = get_joom_product.delay('1473502940450448049-189-2-118-805240694')
+    res = get_joom_product_by_category.delay('1473502940450448049-189-2-118-805240694', '')
+    print(res)
 
 
 
