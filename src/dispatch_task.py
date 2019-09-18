bf12fdd61da8071e3e09ae55baa308d0e73327d1
turpure <p812816627@163.com>
2019-09-18 11:16:00 +0800

    rd reconnection

--- src/dispatch_task.py
+++ src/dispatch_task.py
@@ -33,13 +33,17 @@ def add_task(*args):
 def get_task():
     rd = redis.Redis(**info['redis']['task'])
     while True:
-        task = rd.blpop('joom_task', 30)
-        if not task:
-            continue
-        print('get task {} from task queue'.format(task[1]))
-        task_info = task[1].decode('utf-8').split(',')
-        print(task_info)
-        add_task(*task_info)
+        try:
+            task = rd.blpop('joom_task', 30)
+            if not task:
+                continue
+            print('get task {} from task queue'.format(task[1]))
+            task_info = task[1].decode('utf-8').split(',')
+            print(task_info)
+            add_task(*task_info)
+        except Exception as why:
+            print(f'something wrong because of {why}')
+            rd = redis.Redis(**info['redis']['task'])
 
 
 if __name__ == '__main__':
